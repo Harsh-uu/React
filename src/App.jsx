0945b2750f97e8c387dae5b46f782ffca3ee00cd
Harsh <harshguptaaa17@gmail.com>
2023-02-27 23:28:52 +0530

    Day 2 of react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import User from './User'
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello World!</h1>
+      <User/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/User.jsx
@@ -0,0 +1,7 @@
+function User(){
+    return(
+        <h1>React God Here</h1>
+    )
+}
+
+export default User;
