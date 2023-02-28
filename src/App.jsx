aae9733d54b88d935acc16fe3531754c22dd94ec
Harsh <harshguptaaa17@gmail.com>
2023-02-28 23:39:32 +0530

    added counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   const [num,setNum]=useState(0);
   return (
     <>
-      <h1>Hello World!</h1>
-      <User/>
+      {/* <h1>Hello World!</h1> */}
+      {/* <User/> */}
       <section className='counter'>
       {num}
       <button onClick={()=>{
